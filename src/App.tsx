a38f696f6c17cc14e6744bbcb4242ea9cc0e7f98
Masuda Ryosuke <m1741508@gmail.com>
2025-10-09 23:58:37 +0900

    テスト用ページ作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import { HashRouter, Route, Routes } from 'react-router-dom'
+import List from './list/List'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <HashRouter>
+        <Routes>
+          <Route path='/' element={<div>TopPage coming soon...</div>} />
+          <Route path='/list' element={<List></List>} />
+          <Route path='/test' element={<div>テストページです</div>} />
+        </Routes>
+      </HashRouter>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +36,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
---
+++ src/list/List.tsx
@@ -0,0 +1,8 @@
+import type React from "react";
+
+const List:React.FC = () => {
+  return (
+    <div>作成中</div>
+  )
+}
+export default List;
