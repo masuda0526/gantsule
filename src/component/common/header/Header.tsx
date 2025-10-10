115df71d53ccdfd1295a0b9d00d110f1907742f8
Masuda Ryosuke <m1741508@gmail.com>
2025-10-10 23:41:38 +0900

    ヘッダーレスポンス対応

--- src/component/common/header/Header.tsx
+++ src/component/common/header/Header.tsx
@@ -1,12 +1,47 @@
 import type React from "react";
 import '../../../styles/Header.scss'
 import Logo from "./Logo";
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 const Header: React.FC = () => {
+  // モバイルの境界値
+  const border = 768;
+
+  // モバイルかどうか判定するフラグ
+  const [dispWidth, setDisplayWidth] = useState<number>(window.innerWidth);
+
+  const isMobile = dispWidth < border;
+
+  // マウント時にウィンドウサイズ変更を検知するイベントリスナー
+  useEffect(() => {
+
+    const handleResize = () => {
+      setDisplayWidth(window.innerWidth)
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    // クリーンアップ
+    return () => { window.removeEventListener('resize', handleResize) };
+  }, [])
+
+  const [isActiveMenu, setIsActiveMenu] = useState<boolean>(false);
+
+  const handleClickHum = () => {
+    setIsActiveMenu(!isActiveMenu);
+    console.log(isActiveMenu)
+  }
+
+  const icon = isActiveMenu?(<FontAwesomeIcon icon={faXmark} onClick={handleClickHum}/>):(<FontAwesomeIcon icon={faBars} onClick={handleClickHum}/>)
+
   return (
     <header>
       <Logo></Logo>
-      <nav>
+      {isMobile ? icon : '' }
+      <nav className={isActiveMenu?"is-active":""}>
         <ul>
           <li>TOP</li>
           <li>MENU1</li>
